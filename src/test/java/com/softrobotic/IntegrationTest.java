1918747ca7cb6b7b46474ce2388095efca959c1d
guest001 <habib.h1991@gmail.com>
2023-10-11 00:03:20 +0300

    initial commit

---
+++ src/test/java/com/softrobotic/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.softrobotic;
+
+import com.softrobotic.SoftroboticApp;
+import com.softrobotic.config.AsyncSyncConfiguration;
+import com.softrobotic.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { SoftroboticApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
